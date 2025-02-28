5943460c2f8c396c6fab5b6c7eb4f226fca09e2d
Mark Zhou <149003514+markz03@users.noreply.github.com>
2025-02-28 17:33:31 +0000

    }{

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -3,7 +3,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Deck cards = new Deck();
-        System.out.println(cards.getCards());
+        Player player = new Player();
+        player.addCard(new Card("10", "♠"));
+        player.addCard(new Card("J", "♠"));
+        
+        // Community Cards
+        ArrayList<Card> communityCards = new ArrayList<>();
+        communityCards.add(new Card("Q", "♠"));
+        communityCards.add(new Card("K", "♠"));
+        communityCards.add(new Card("A", "♠"));
+        
+        player.playHand(communityCards);
+        String handResult = player.playHand(communityCards);
+        System.out.println(handResult);
     }
 }
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -12,6 +12,7 @@ public class Player{
     
     public Player(){
         hand = new ArrayList<>();
+        allCards = new ArrayList<>();
     }
 
     public ArrayList<Card> getHand(){return hand;}
@@ -19,23 +20,75 @@ public class Player{
 
     public void addCard(Card c){
         hand.add(c);
-        allCards.add(c);
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
+    public String playHand(ArrayList<Card> communityCards){
+        allCards = new ArrayList<>();
+        for (int i = 0; i < communityCards.size(); i++) {
+            allCards.add(communityCards.get(i));
+        }
+        for (int j = 0; j < hand.size(); j++) {
+            allCards.add(hand.get(j));
+        }
+
+        sortAllCards();
+
+        ArrayList<Integer> rankFrequency = findRankingFrequency();
+        ArrayList<Integer> suitFrequency = findSuitFrequency();
+        System.out.println(rankFrequency);
+        System.out.println(suitFrequency);
+
+        for (int i = 0; i < suitFrequency.size(); i++) {
+            if (suitFrequency.get(i) == 5) {
+                if (checkRoyalFlush(rankFrequency)) {
+                    return "Royal Flush";
+                }
+            }
+        }
+
         return "Nothing";
     }
 
     public void sortAllCards(){
-        allCards = Arrays.sort(allCards);
+        for (int i = 1; i < allCards.size(); i++) {
+            int j = i - 1;
+            while (j > -1 && Utility.getRankValue(allCards.get(j).getRank()) > Utility.getRankValue(allCards.get(j + 1).getRank())) {
+                Card temp = allCards.set(j + 1, allCards.get(j));
+                allCards.set(j, temp);
+                j--;
+            }
+        }
     } 
 
     public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+        ArrayList<Integer> rankFrequency = new ArrayList<>();
+
+        for (int i = 0; i < ranks.length; i++) {
+            int frequency = 0;
+            for (int j = 0; j < allCards.size(); j++) {
+                if (Utility.getRankValue(ranks[i]) == Utility.getRankValue(allCards.get(j).getRank())) {
+                    frequency++;
+                }
+            }
+            rankFrequency.add(frequency);
+        }
+
+        return rankFrequency; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+        // {♠, ♥, ♣, ♦} order
+        ArrayList<Integer> suitFrequency = new ArrayList<>();
+        for (int i = 0; i < suits.length; i++) {
+            int frequency = 0;
+            for (int j = 0; j < allCards.size(); j++) {
+                if (suits[i].equals(allCards.get(j).getSuit())) {
+                    frequency++;
+                }
+            }
+            suitFrequency.add(frequency);
+        }
+        return suitFrequency; 
     }
 
    
@@ -44,6 +97,33 @@ public class Player{
         return hand.toString();
     }
 
+    public Boolean checkRoyalFlush(ArrayList<Integer> rankFrequency) {
+        int streak = 0;
+        for (int j = 8; j < rankFrequency.size(); j++) {
+            if (rankFrequency.get(j) == 1) {
+                streak++;
+            }
+        }
+
+        if (streak == 5) {
+            return true;
+        }
+        return false;
+    }
+
+    Public Boolean checkStraightFlush(ArrayList<Integer> rankFrequency) {
+        int streak = 0;
+        int maxStreak = 0;
+        for (int j = 0; j < rankFrequency.size(); j++) {
+            if (rankFrequency.get(j) == 5) {
+                streak++;
+            }
+            else {
+                maxStreak = streak;
+                streak = 0;
+            }
+        }
+    }
 
 
 
