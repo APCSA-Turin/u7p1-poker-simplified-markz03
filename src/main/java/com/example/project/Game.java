f5858f42f62b2a70ab0dead76b7d7c2e2d39fd54
Mark Zhou <149003514+markz03@users.noreply.github.com>
2025-03-06 00:15:33 +0000

    finver&comments

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -4,11 +4,37 @@ import java.util.ArrayList;
 
 public class Game{
     public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
+        // int values of Hand Ranking
+        int p1Ranking = Utility.getHandRanking(p1Hand);
+        int p2Ranking = Utility.getHandRanking(p2Hand);
+
+        // Player 1 has a higher hand, therefore Player 1 wins
+        if (p1Ranking > p2Ranking) {
+            return "Player 1 wins!";
+        }
+        // Player 2 has a higher hand, therefore Player 1 wins
+        else if (p2Ranking > p1Ranking) {
+            return "Player 2 wins!";
+        }
+        // if they have the same hand ranking
+        else if (p1Ranking == p2Ranking) {
+            // checks the highest card in each player's hand, the higher one wins, else if equal = tie
+            if (p1.highestCard(p1.getHand()) > p2.highestCard(p2.getHand())) {
+                return "Player 1 wins!";
+            }
+            else if (p2.highestCard(p2.getHand()) > p1.highestCard(p1.getHand())) {
+                return "Player 2 wins!";
+            }
+            else {
+                return "Tie!";
+            }
+        }
+
         return "Error";
     }
 
     public static void play(){ //simulate card playing
-    
+        
     }
         
         
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -12,7 +12,6 @@ public class Player{
     
     public Player(){
         hand = new ArrayList<>();
-        allCards = new ArrayList<>();
     }
 
     public ArrayList<Card> getHand(){return hand;}
@@ -35,21 +34,60 @@ public class Player{
 
         ArrayList<Integer> rankFrequency = findRankingFrequency();
         ArrayList<Integer> suitFrequency = findSuitFrequency();
-        System.out.println(rankFrequency);
-        System.out.println(suitFrequency);
 
-        for (int i = 0; i < suitFrequency.size(); i++) {
-            if (suitFrequency.get(i) == 5) {
-                if (checkRoyalFlush(rankFrequency)) {
-                    return "Royal Flush";
-                }
+        // Only if 5 consecutive numbers in rankFrequency
+        if (maxStreak(rankFrequency) == 5) {
+            if (checkRoyalFlush(rankFrequency)) {
+                return "Royal Flush";
+            }
+            else if (checkStraightFlush(suitFrequency)) {
+                return "Straight Flush";
+            }
+            else if (checkStraight(suitFrequency)){
+                return "Straight";
             }
         }
+        // Only if all cards are of the same suit
+        else if (highestFrequency(suitFrequency) == 5) {
+            return "Flush";
+        }
+        // Only if a rank appears 4 times in allCards
+        else if (highestFrequency(rankFrequency) == 4) {
+            if (checkFourOfAKind(rankFrequency)) {
+                return "Four of a Kind";
+            }
+        }
+        // Only if a rank appears 3 times in AllCards
+        else if (highestFrequency(rankFrequency) == 3) {
+            // Full House if a pair is found, else it's just a Three of a Kind
+            if (checkPair(rankFrequency) == 1) {
+                return "Full House";
+            }
+            else {
+                return "Three of a Kind";
+            }
+        }
+        // Only if one or more pair is detected in AllCards
+        else if (checkPair(rankFrequency) > 0) {
+            // If there are Two or One Pair in allCards
+            if (checkPair(rankFrequency) == 2) {
+                return "Two Pair";
+            }
+            else {
+                return "A Pair";
+            }
+        }
+        // Only if player's hand has a card greater than the highest Card in the communityCards
+        else if (highestCard(hand) > highestCard(communityCards)) {
+            return "High Card";
+        }
 
+        // Only if everything else is false, the player's handRanking is nothing
         return "Nothing";
     }
 
     public void sortAllCards(){
+        // Insertion Sort
         for (int i = 1; i < allCards.size(); i++) {
             int j = i - 1;
             while (j > -1 && Utility.getRankValue(allCards.get(j).getRank()) > Utility.getRankValue(allCards.get(j + 1).getRank())) {
@@ -62,7 +100,8 @@ public class Player{
 
     public ArrayList<Integer> findRankingFrequency(){
         ArrayList<Integer> rankFrequency = new ArrayList<>();
-
+        
+        // Iterates through AllCards to find the frequency of each Card's rank and adds it into rankFrequency
         for (int i = 0; i < ranks.length; i++) {
             int frequency = 0;
             for (int j = 0; j < allCards.size(); j++) {
@@ -73,12 +112,15 @@ public class Player{
             rankFrequency.add(frequency);
         }
 
+        // returns an ArrayList<Integer> rankFrequency that contains the frequency of each card's rank in AllCards
         return rankFrequency; 
     }
 
     public ArrayList<Integer> findSuitFrequency(){
         // {♠, ♥, ♣, ♦} order
         ArrayList<Integer> suitFrequency = new ArrayList<>();
+
+        // Iterates through AllCards to find the frequency of each Card's suit and adds it into suitFrequency
         for (int i = 0; i < suits.length; i++) {
             int frequency = 0;
             for (int j = 0; j < allCards.size(); j++) {
@@ -88,16 +130,14 @@ public class Player{
             }
             suitFrequency.add(frequency);
         }
+        
+        // returns an ArrayList<Integer> suitFrequency that contains the frequency of each card's suit in AllCards
         return suitFrequency; 
     }
 
-   
-    @Override
-    public String toString(){
-        return hand.toString();
-    }
-
     public Boolean checkRoyalFlush(ArrayList<Integer> rankFrequency) {
+        // checks if player has a Royal Flush via rankFrequency
+        // True when the player has a {10, J, Q, K, A}
         int streak = 0;
         for (int j = 8; j < rankFrequency.size(); j++) {
             if (rankFrequency.get(j) == 1) {
@@ -111,20 +151,97 @@ public class Player{
         return false;
     }
 
-    Public Boolean checkStraightFlush(ArrayList<Integer> rankFrequency) {
+    public Boolean checkStraightFlush(ArrayList<Integer> suitFrequency) {
+        // checks if player has a Straight Flush via suitFrequency
+        // True when allCards has 5 cards of the same suit (5 consecutive ranks are checked in playHand)
+        if (highestFrequency(suitFrequency) == 5) {
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean checkFourOfAKind(ArrayList<Integer> rankFrequency) {
+        // checks if the player has a Four of a Kind via rankFrequency
+        // True if allCards contains 4 cards of the same rank
+        if (highestFrequency(rankFrequency) == 4) {
+            return true;
+        }
+        return false;
+    }
+
+    public int checkPair(ArrayList<Integer> rankFrequency) {
+        // checks the amount of pairs in AllCards and returns it
+        int pairCount = 0;
+        for (int i = 0; i < rankFrequency.size(); i++) {
+            if (rankFrequency.get(i) == 2) {
+                pairCount++;
+            }
+        }
+        return pairCount;
+    }
+
+    public boolean checkStraight(ArrayList<Integer> suitFrequency) {
+        // Checks if the player has a Straight (5 consective ranks has already been checked in playHand)
+        // true if AllCards contains 5 consecutive ranks of any suit
+        if (highestFrequency(suitFrequency) != 5) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int highestCard(ArrayList<Card> hand) {
+        // determines the player's highest rank value in their hand
+        int highestRank = 0;
+        int rank = 0;
+
+        for (int i = 0; i < hand.size(); i++) {
+            rank = Utility.getRankValue(hand.get(i).getRank());
+            if (rank > highestRank) {
+                highestRank = rank;
+            }
+        }
+
+        return highestRank;
+    }
+
+    public int maxStreak(ArrayList<Integer> arrayFrequency) {
+        // Determines and returns the highest consecutive streak
+        // Used exclusively on rankFrequency as the parameter
         int streak = 0;
-        int maxStreak = 0;
-        for (int j = 0; j < rankFrequency.size(); j++) {
-            if (rankFrequency.get(j) == 5) {
+        int maxStreak = 0; 
+
+        for (int i = 0; i < arrayFrequency.size(); i++) {
+            if (arrayFrequency.get(i) != 0) {
                 streak++;
+                if (streak > maxStreak) {
+                    maxStreak = streak;
+                }
             }
             else {
-                maxStreak = streak;
                 streak = 0;
             }
         }
+
+        return maxStreak;
     }
 
+    public int highestFrequency(ArrayList<Integer> arrayFrequency) {
+        // Determines and returns g that is the greatest in arrayFrequency
+        // Used by both rankFrequency and suitFrequency
+        int highestFreq = 0;
 
+        for (int i = 0; i < arrayFrequency.size(); i++) {
+            if (arrayFrequency.get(i) > highestFreq) {
+                highestFreq = arrayFrequency.get(i);
+            }
+        }
 
+        return highestFreq;
+    }
+
+    @Override
+    public String toString(){
+        return hand.toString();
+    }
 }
